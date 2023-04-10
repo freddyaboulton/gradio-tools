9630531fd287aa98cb72a95c408652bffc1849f6
freddyaboulton <alfonsoboulton@gmail.com>
2023-04-10 15:12:54 -0400

    Add test script

---
+++ gradio_tool/test_script.py
@@ -0,0 +1,23 @@
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
+import os
+from gradio_tool.tool import StableDiffusionTool, ImageCaptioningTool, ImageToMusicTool
+from langchain.memory import ConversationBufferMemory
+
+
+os.environ["OPENAI_API_KEY"] = "<Secret Key>"
+
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
+import os
+from gradio_tool.tool import StableDiffusionTool, ImageCaptioningTool, ImageToMusicTool
+from langchain.memory import ConversationBufferMemory
+
+llm = OpenAI(temperature=0)
+memory = ConversationBufferMemory(memory_key="chat_history")
+tools = [StableDiffusionTool(), ImageCaptioningTool(), ImageToMusicTool()]
+
+
+agent = initialize_agent(tools, llm, memory=memory, agent="conversational-react-description", verbose=True)
+output = agent.run(input=("I would please like a photo of a dog riding a skateboard. "
+                          "Please caption this image and create a song for it."))
