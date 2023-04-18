872d247e477eafcb97f1919d31371c9dbc145fef
Abubakar Abid <abubakar@huggingface.co>
2023-04-18 09:42:17 -0700

    Adding the DocQuery tool

--- gradio_tools/tools.py
+++ gradio_tools/tools.py
@@ -183,6 +183,27 @@ class ImageCaptioningTool(GradioTool):
         return gr.Textbox()
 
 
+class DocQueryDocumentAnsweringTool(GradioTool):
+    def __init__(
+        self,
+        name="DocQuery",
+        description=(
+            "A tool for answering questions about a document from the from the image of the document. Input will be a two strings separated by a comma: the first will be the path or URL to an image of a document. The second will be your question about the document."
+            "The output will the text answer to your question."
+        ),
+        src="abidlabs/docquery",
+        hf_token=None,
+    ) -> None:
+        super().__init__(name, description, src, hf_token)
+
+    def create_job(self, query: str) -> Job:
+        img, question = query.split(",")
+        return self.client.submit(img.strip(), question.strip() api_name="/predict")
+
+    def postprocess(self, output: str) -> str:
+        return output
+
+
 class ImageToMusicTool(GradioTool):
     def __init__(
         self,
@@ -237,6 +258,7 @@ class WhisperTool(GradioTool):
 
     def _block_output(self) -> "gr.components.Component":
         return gr.Textbox()
+
 
 
 class StableDiffusionPromptGeneratorTool(GradioTool):
