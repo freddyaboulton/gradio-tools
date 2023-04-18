a20dbddcda8bf7d4080dd2680f9ca82bdc911867
freddyaboulton <alfonsoboulton@gmail.com>
2023-04-17 21:52:02 -0400

    Add auto duplicate

--- gradio_tools/tools.py
+++ gradio_tools/tools.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import time
 from abc import abstractmethod
@@ -5,6 +7,7 @@ from functools import wraps
 from typing import Any, Tuple, Union
 
 import gradio_client as grc
+import huggingface_hub
 from gradio_client.client import Job
 from gradio_client.utils import QueueError
 
@@ -33,17 +36,31 @@ def raises_without_gradio(f):
 
 
 class GradioTool:
-    def __init__(self, name: str, description: str, src: str) -> None:
+    def __init__(
+        self, name: str, description: str, src: str, hf_token: str | None = None
+    ) -> None:
         self.name = name
         self.description = description
-        self.src = src
-        self.client = grc.Client(self.src)
+        if hf_token and self._is_space(src):
+            self.client = grc.Client.duplicate(from_id=src, hf_token=hf_token)
+            self.src = self.client.space_id
+        else:
+            self.src = src
+            self.client = grc.Client(self.src)
         self.description += (
             "If the output is the string QUEUE_FULL you "
             "should try this action again and never stop until you "
             "don't see this message."
         )
         self._block = None
+
+    @staticmethod
+    def _is_space(src: str) -> bool:
+        try:
+            huggingface_hub.get_space_runtime(src)
+            return True
+        except huggingface_hub.hf_api.RepositoryNotFoundError:
+            return False
 
     @abstractmethod
     def create_job(self, query: str) -> Job:
@@ -115,8 +132,9 @@ class StableDiffusionTool(GradioTool):
             "look like. The output will be a path to an image file."
         ),
         src="gradio-client-demos/stable-diffusion",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(query, "", 9, fn_index=1)
@@ -148,8 +166,9 @@ class ImageCaptioningTool(GradioTool):
             "The output will be a caption of that image."
         ),
         src="gradio-client-demos/comparing-captioning-models",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(query.strip("'"), fn_index=0)
@@ -174,8 +193,9 @@ class ImageToMusicTool(GradioTool):
             "The output will be an audio file generated from that image."
         ),
         src="fffiloni/img-to-music",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(
@@ -202,13 +222,14 @@ class WhisperTool(GradioTool):
             "The output will the text transcript of that file."
         ),
         src="abidlabs/whisper-large-v2",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(query, api_name="/predict")
 
-    def postprocess(self, output: Union[Tuple[Any], Any]) -> str:
+    def postprocess(self, output: str) -> str:
         return output
 
     def _block_input(self) -> "gr.components.Component":
@@ -229,13 +250,14 @@ class StableDiffusionPromptGeneratorTool(GradioTool):
             "and the output is a prompt text string"
         ),
         src="microsoft/Promptist",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(query, api_name="/predict")
 
-    def postprocess(self, output: Union[Tuple[Any], Any]) -> str:
+    def postprocess(self, output: str) -> str:
         return output
 
 
@@ -249,15 +271,16 @@ class ClipInterrogatorTool(GradioTool):
             "input image. The imput is a path to an image. The output is a text string."
         ),
         src="pharma/CLIP-Interrogator",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(
             query, "ViT-L (best for Stable Diffusion 1.*)", "best", fn_index=3
         )
 
-    def postprocess(self, output: Union[Tuple[Any], Any]) -> str:
+    def postprocess(self, output: str) -> str:
         return output
 
     def _block_input(self) -> "gr.components.Component":
@@ -275,13 +298,14 @@ class TextToVideoTool(GradioTool):
             "The output will be a path to a video file."
         ),
         src="damo-vilab/modelscope-text-to-video-synthesis",
+        hf_token=None,
     ) -> None:
-        super().__init__(name, description, src)
+        super().__init__(name, description, src, hf_token)
 
     def create_job(self, query: str) -> Job:
         return self.client.submit(query, -1, 16, 25, fn_index=1)
 
-    def postprocess(self, output: Union[Tuple[Any], Any]) -> str:
+    def postprocess(self, output: str) -> str:
         return output
 
     def _block_output(self) -> "gr.components.Component":
